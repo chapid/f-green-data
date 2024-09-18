9416fad3c8057cb33d99bb55944eaaf0e41a7ccc
Melizza Cely <melizza.cely@gmail.com>
2024-09-17 21:17:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home page</h1>} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/register" element={<h1>Register</h1>} />
+      <Route path="/profile" element={<h1>Profile</h1>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
